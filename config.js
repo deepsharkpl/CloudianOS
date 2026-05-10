fe29a4b8863e7c3d157249aa78ee2c8a45c54cb6
monderey <burzynsonek@gmail.com>
2026-05-10 22:49:35 +0200

    added: File downloading information about nodejs, npm, cloudian version

---
+++ config.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "package.json"), "utf8")
+);
+
+function getNpmVersion() {
+  try {
+    return execSync("npm -v").toString().trim();
+  } catch (err) {
+    return "unknown";
+  }
+}
+
+const config = {
+  version: packageJson.version,
+  nodeVersion: process.version,
+  npmVersion: getNpmVersion(),
+};
+
+module.exports = config;
