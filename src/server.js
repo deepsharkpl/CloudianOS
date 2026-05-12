52e19ac8450697ef76c5a385222868f1dba30197
monderey <burzynsonek@gmail.com>
2026-05-12 14:52:58 +0200

    feat(i18n): set up global language support across project

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+const { i18nMiddleware } = require("./i18n/i18n");
+
 const app = express();
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -10,6 +12,8 @@ app.set("views", path.join(__dirname, "UI"));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(i18nMiddleware);
 
 app.use("/styles", express.static(path.join(__dirname, "styles", "css")));
 app.use("/img", express.static(path.join(__dirname, "styles", "img")));
