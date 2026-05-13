f0b8c1a8ee80c4d0f79116d9c39d11e57aefa155
monderey <burzynsonek@gmail.com>
2026-05-13 21:20:36 +0200

    feat(cli): redesign server startup output

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
-require('dotenv').config();
+require('dotenv').config({
+  quiet: true,
+});
+
 const express = require('express');
 const path = require('path');
+const config = require('../config');
+
+const os = require('os');
 
 const { i18nMiddleware } = require('./i18n/i18n');
+const chalk = require('chalk');
+
+const centerBoxText = require('./utils/text/centerBoxText');
+const line =
+  '╔═════════════════════════════════════════════════════════════════╗';
+const separator =
+  '║─────────────────────────────────────────────────────────────────║';
+const width = line.length;
 
 const app = express();
 const PORT = process.env.HTTP_PORT || 3000;
@@ -30,6 +44,91 @@ app.get('/test', (req, res) => {
 app.use('/api/configuration', require('./routes/api.configuration'));
 app.use('/api/system', require('./routes/api.system'));
 
-app.listen(PORT, () => {
-  console.log(`The server is running on http://localhost:${PORT}`);
-});
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+
+  return '127.0.0.1';
+}
+
+function startServer(port) {
+  const localIP = getLocalIP();
+  app
+    .listen(port, () => {
+      console.log(
+        '╔═════════════════════════════════════════════════════════════════╗',
+      );
+      console.log(centerBoxText(' ', width));
+      console.log(
+        centerBoxText(
+          chalk.green(
+            `Blueberry OS v${config.version} has been successfully launched.`,
+          ),
+          width,
+        ),
+      ),
+      console.log(centerBoxText(' ', width));
+      console.log(
+        centerBoxText(
+          `Blueberry is now live at · ` +
+            chalk.underline.blue(`http://localhost:${port}`),
+          width,
+        ),
+      );
+      console.log(
+        centerBoxText(
+          `Network access · ` +
+            chalk.underline.green(`http://${localIP}:${port}`),
+          width,
+        ),
+      );
+      console.log(centerBoxText(' ', width));
+      console.log(separator);
+      console.log(centerBoxText(' ', width));
+      console.log(
+        centerBoxText(
+          chalk.gray('Blueberry OS is still under active development.'),
+          width,
+        ),
+      );
+      console.log(
+        centerBoxText(
+          chalk.gray('Some features or apps may contain bugs/issues.'),
+          width,
+        ),
+      );
+      console.log(
+        centerBoxText(
+          chalk.gray('Join devs & community: ') +
+            chalk.underline.gray('https://discord.gg/kmEsF6PSkt'),
+          width,
+        ),
+      );
+      console.log(centerBoxText(' ', width));
+      console.log(
+        '╚═════════════════════════════════════════════════════════════════╝\n',
+      );
+    })
+    .on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        console.error(
+          chalk.red(
+            '[ ' + chalk.red('FAIL') + ` ]  Port ${port} is already in use. Trying next port...`,
+          ),
+        );
+
+        startServer(port + 1);
+      } else {
+        console.error(err);
+      }
+    });
+}
+
+startServer(PORT);
